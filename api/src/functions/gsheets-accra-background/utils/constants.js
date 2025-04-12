1a49aa25ee38d0c4c79746323699e195271eabef
John-Dag <jaedagy@gmail.com>
2025-04-12 03:49:16 +0000

    refactor: remove gsheets-accra-background function files and related constants

--- api/src/functions/gsheets-accra-background/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const CAMPUS_NAME = 'Accra'
-export const notifyBaseURL = 'https://api-notify.firstlovecenter.com'
-
-export const lastSunday = new Date(
-  new Date().setDate(new Date().getDate() - new Date().getDay())
-)
-  .toISOString()
-  .split('T')[0]
