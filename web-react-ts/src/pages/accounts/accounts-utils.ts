817cf213b7f6a590420750140e955585c7fcfd2c
John-Dag <jaedagy@gmail.com>
2025-10-01 05:17:37 +0000

    refactor: simplify account open logic by removing unused day checks

--- web-react-ts/src/pages/accounts/accounts-utils.ts
+++ web-react-ts/src/pages/accounts/accounts-utils.ts
@@ -1,16 +1,11 @@
 export const isAccountOpen = () => {
   const currentDate = new Date()
-  const currentDay = currentDate.getUTCDay()
+  // const currentDay = currentDate.getUTCDay()
   const currentHour = currentDate.getUTCHours()
 
-  // From Sunday (0) to Wednesday (3), always return true
-  if (currentDay >= 0 && currentDay <= 3) {
-    return true
-  }
-
-  // For Thursday (4), return true only before 10am
-  if (currentDay === 4 && currentHour < 10) {
-    return true
+  // Accounts are only open from 6am to 3pm daily (15:00 in 24-hour format)
+  if (currentHour < 6 || currentHour >= 15) {
+    return false
   }
 
   // For Thursday after 10am, all of Friday (5), and Saturday (6), return false
